87ee14b6d07133fc372898b6aa023b8d886f6a41
strandmoscow <abc010801@mail.ru>
2023-05-07 18:46:56 +0300

    Done new forms for registration

---
+++ phoenix/config_vars.py
@@ -0,0 +1,15 @@
+
+# DEVELOPMENT KEY
+DEV_SECRET_KEY = 'your_secret_key'
+
+# TESTING KEY
+TEST_SECRET_KEY = 'test_secret_key'
+
+# PRODUCTION KEY
+SECRET_KEY = 'your_production_key'
+
+# DATABASE SETTINGS
+DB_HOST = '70.34.250.137'
+DB_USER = 'postgres'
+DB_PASS = 'p_admin_p'
+DB_NAME = 'Phoenix'
