e6a5de5366800b50ac23db8b98e3acbc7aba9436
strandmoscow <abc010801@mail.ru>
2023-05-07 15:19:03 +0300

    Initial commit

--- phoenix/main/__init__.py
+++ phoenix/main/__init__.py
@@ -13,13 +13,6 @@ def index():
     return render_template('index.html')
 
 
-@main.route("/create_db")
-def create_db():
-    conn.create_all()
-    flash('Database created successfully!', 'info')
-    return redirect(url_for('main.home'))
-
-
 @current_app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html', title="Oops! Page Not Found", error=error), 404
