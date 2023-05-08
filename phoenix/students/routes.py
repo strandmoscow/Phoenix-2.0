852e290f67de3a14d7b0ed09401d4b09dfb07076
Аnastasia <fenix2704@bk.ru>
2023-05-09 02:44:58 +0300

    super group in students table new blank group page

--- phoenix/students/routes.py
+++ phoenix/students/routes.py
@@ -2,9 +2,9 @@ from flask import Blueprint, redirect, render_template, session, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from ..registration.models import account as accountdata
 from .models import students as studentsdata
+from ..groups.models import group as groupdata
 from .. import db, auth
 from flask_login import login_user, login_required, current_user
-from ..auth.userLogin import UserLogin
 
 students = Blueprint('students', __name__, template_folder='templates', static_folder='static')
 
@@ -12,6 +12,10 @@ students = Blueprint('students', __name__, template_folder='templates', static_f
 @students.route("/student", methods=['GET', 'POST'])
 @login_required
 def student():
-    accs = accountdata.query.filter(accountdata.account_student_id != None).all()
-    studentstable = studentsdata.query.all()
-    return render_template('students/students.html', studentstable=studentstable, accs=accs, cu=current_user.get_id())
+    accs = db.session.query(accountdata.account_surname, accountdata.account_name, accountdata.account_patronymic, studentsdata.student_health_insurance, studentsdata.student_birth_certificate, studentsdata.student_snils, groupdata.group_name).\
+        join(studentsdata, accountdata.account_student_id == studentsdata.student_id).\
+        join(groupdata, studentsdata.student_group_id == groupdata.group_id).all()
+
+    # accs = accountdata.query.filter(accountdata.account_student_id != None).all()
+
+    return render_template('students/students.html', accs=accs, cu=current_user.get_id())
