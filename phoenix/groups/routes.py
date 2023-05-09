c31d96a4c224eeb2b2c075ab3afb864cb975fc13
Аnastasia <fenix2704@bk.ru>
2023-05-09 19:04:48 +0300

    new single group page

--- phoenix/groups/routes.py
+++ phoenix/groups/routes.py
@@ -13,11 +13,37 @@ groups = Blueprint('groups', __name__, template_folder='templates', static_folde
 @login_required
 def group():
     groups = db.session.query(groupdata.group_name, accountdata.account_id, accountdata.account_surname,
-                              accountdata.account_name, db.func.count(students.student_id)) \
+                              accountdata.account_name, db.func.count(students.student_id), groupdata.group_id) \
         .join(trainer, groupdata.group_trainer_id == trainer.trainer_id) \
         .join(accountdata, trainer.trainer_id == accountdata.account_trainer_id) \
         .outerjoin(students, groupdata.group_id == students.student_group_id) \
-        .group_by(groupdata.group_name, accountdata.account_id, accountdata.account_surname, accountdata.account_name) \
+        .group_by(groupdata.group_name, accountdata.account_id, accountdata.account_surname,
+                  accountdata.account_name, groupdata.group_id) \
         .all()
 
     return render_template('groups/groups.html', groups=groups, cu=current_user.get_id())
+
+
+@groups.route("/group/<int:group_id>", methods=['GET', 'POST'])
+@login_required
+def singlegroup(group_id):
+    gr = groupdata.query.get(group_id)
+    grinf = db.session.query(groupdata.group_name, accountdata.account_id, accountdata.account_surname,
+                             accountdata.account_name, accountdata.account_patronymic) \
+        .join(trainer, groupdata.group_trainer_id == trainer.trainer_id) \
+        .join(accountdata, trainer.trainer_id == accountdata.account_trainer_id) \
+        .filter(groupdata.group_id == group_id) \
+        .all()
+    sts = db.session.query(accountdata.account_id, accountdata.account_surname, accountdata.account_name,
+                           accountdata.account_patronymic). \
+        join(students, accountdata.account_student_id == students.student_id). \
+        join(groupdata, students.student_group_id == groupdata.group_id) \
+        .filter(groupdata.group_id == group_id) \
+        .all()
+
+    num_students = db.session.query(students.student_id) \
+        .join(groupdata, groupdata.group_id == students.student_group_id) \
+        .filter(groupdata.group_id == group_id) \
+        .count()
+    return render_template('groups/group.html', group=gr, grinf=grinf, students=sts, num_students=num_students,
+                           cu=current_user.get_id())
