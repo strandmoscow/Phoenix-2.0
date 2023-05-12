2c942c6d3e0f021b8f69edb38742feeafda7cedd
strandmoscow <abc010801@mail.ru>
2023-05-12 13:09:53 +0300

    Validation beta

--- phoenix/account/forms.py
+++ phoenix/account/forms.py
@@ -5,6 +5,7 @@ from wtforms.validators import Email, DataRequired, Length, EqualTo
 class AccountForm1(FlaskForm):
     surname = StringField("Фамилия", validators=[DataRequired()])
     firstname = StringField("Имя", validators=[DataRequired()])
+    patronymic = StringField("Отчество", validators=[DataRequired()])
     email = StringField("Email", validators=[Email(), DataRequired()])
     dob = DateField("Дата рождения", validators=[DataRequired()])
     phone = StringField("Номер телефона", validators=[DataRequired()])
