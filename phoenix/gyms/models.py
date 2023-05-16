d4733e9b8f01907b15bb91e066912e66edef80e3
Аnastasia <fenix2704@bk.ru>
2023-05-16 16:37:13 +0300

    new gym add, edit and delete

--- phoenix/gyms/models.py
+++ phoenix/gyms/models.py
@@ -4,12 +4,12 @@ from .. import db
 class Gym(db.Model):
     gym_id = db.Column(db.Integer, primary_key=True)
     gym_name = db.Column(db.String(64), nullable=False)
-    building_building_id = db.Column(db.Integer, db.ForeignKey('building.building_id'), nullable=False)
+    gym_building_id = db.Column(db.Integer, db.ForeignKey('building.building_id'), nullable=False)
 
     building = db.relationship('Building', backref='gym')
 
     def __repr__(self):
-        return f'<gym {self.gym_id}>'
+        return f'<Gym {self.gym_id}>'
 
 
 class Building(db.Model):
@@ -22,4 +22,4 @@ class Building(db.Model):
     club = db.relationship("Club")
 
     def __repr__(self):
-        return f'<building {self.building_id}>'
+        return f'<Building {self.building_id}>'
