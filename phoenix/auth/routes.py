aa79adfed52af7a2501de3aab43b602c6781b503
strandmoscow <abc010801@mail.ru>
2023-05-18 00:54:03 +0300

    Remember me

--- phoenix/auth/routes.py
+++ phoenix/auth/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, session, request, url_for
+from flask import Blueprint, redirect, render_template, session, request, url_for, flash
 from werkzeug.security import check_password_hash
 from flask_login import login_user, logout_user, current_user, user_logged_in
 from .. import db, login_manager, logout_required
@@ -23,10 +23,11 @@ def auth():
     if form.validate_on_submit():
         user = Account.query.filter_by(account_email=form.email.data).first()
         if user and check_password_hash(user.account_password, form.password.data):
-            print(str(user.account_id))
             userLogin = UserLogin().create(user)
-            login_user(userLogin)
-            print(f"User {userLogin.get_id()} logged in")
+            if form.remember.data:
+                login_user(userLogin, remember=True)
+            else:
+                login_user(userLogin)
             return redirect(url_for('main.index'))
         else:
             print("Some problems")
@@ -36,9 +37,12 @@ def auth():
 
 
 @authentication.route("/exit")
-@logout_required
 def exit():
     logout_user()
+    if session.get('was_once_logged_in'):
+        # prevent flashing automatically logged out message
+        del session['was_once_logged_in']
+    flash('You have successfully logged yourself out.')
     return redirect(url_for('main.index'))
 
 
